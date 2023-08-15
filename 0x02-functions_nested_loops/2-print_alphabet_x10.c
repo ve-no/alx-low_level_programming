12a641c605a258085086dde97b878a1892a09812
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2023-08-15 12:16:29 +0100

    print_alphabet_x10 - prints 10 times the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	for (int i = 0; i < 10; i++) {
+		for (char c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
