05159e948bc9a51588a67a8836096ebf21570ffa
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2023-08-15 13:10:21 +0100

    print_sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/***
+* print_sign - prints the sign of a number
+* @n: number to be checked
+* Return: 1 if n is positive, -1 if n is negative, 0 if n is zero
+*/
+int	print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
