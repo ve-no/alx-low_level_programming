f5295bf76c1fa4af1db5f938b9fefcd11ee18ad9
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2023-08-15 16:04:12 +0100

    simple change

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,23 +10,21 @@ void print_times_table(int n)
 	int i, j, k;
 
 
-	if (n >= 0 && n <= 15)
+	for (i = 0; i <= n; i++)
 	{
-		for (i = 0; i <= n; i++)
+		for (j = 0; j <= n; j++)
 		{
-			for (j = 0; j <= n; j++)
-			{
-				k = j * i;
-				if (j == 0)
-					printf("%d", k);
-				else if (k < 10 && j != 0)
-					printf(",   %d", k);
-				else if (k >= 10 && k < 100)
-					printf(",  %d", k);
-				else if (k >= 100)
-					printf(", %d", k);
-			}
-			printf("\n");
+			k = j * i;
+			if (j == 0)
+				printf("%d", k);
+			else if (k < 10 && j != 0)
+				printf(",   %d", k);
+			else if (k >= 10 && k < 100)
+				printf(",  %d", k);
+			else if (k >= 100)
+				printf(", %d", k);
 		}
+		printf("\n");
 	}
+
 }
