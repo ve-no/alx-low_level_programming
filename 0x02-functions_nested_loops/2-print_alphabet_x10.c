63348ccf6e43ba15795815ac86e0795446b68ee9
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2023-08-15 12:21:02 +0100

    norm

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,8 @@ void print_alphabet_x10(void)
 	int i;
 
 	for (i = 0; i < 10; i++) {
-		for (char c = 'a'; c <= 'z'; c++)
+		char c;
+		for (c = 'a'; c <= 'z'; c++)
 			_putchar(c);
 		_putchar('\n');
 	}
