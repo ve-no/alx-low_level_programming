a88836445033f92c30f59e2b2a411c3899b03044
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2023-08-15 13:02:20 +0100

    fix an err

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,7 @@
  * @c : character to be checked
  * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
  */
-int	isalpha(int c)
+int	_isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
