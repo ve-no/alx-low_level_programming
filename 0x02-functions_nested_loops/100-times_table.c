5c982473b174bc95d2d96c303c3d051b0a01d9fc
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2023-08-15 16:02:05 +0100

    simple modification

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,26 +4,57 @@
 * print_times_table - prints the n times table, starting with 0
 * @n: number of the times table
 */
+// void print_times_table(int n)
+// {
+// 	int i, j, k;
+// 	for(i = 0; i <= n; i++)
+// 	{
+// 		for (j = 0; j <= n; j++)
+// 		{
+// 			k = i * j;
+// 			if(j != n)
+// 			{
+// 				if (j == 0)
+// 					printf("%d,", k);
+// 				else if(k < 10 && j != 0)
+// 					printf("%d,   ", k);
+// 				else if(k >= 10 && k < 100)
+// 					printf("%d,  ", k);
+// 				else
+// 					printf("%d, ", k);
+// 			}
+// 			else
+// 				printf("%d", k);
+// 		}
+// 		printf("\n");
+// 	}
+// }
+
+
+
+
 void print_times_table(int n)
 {
 	int i, j, k;
-	for(i = 0; i <= n; i++)
+
+
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			k = i * j;
-			if(j != n)
+			for (j = 0; j <= n; j++)
 			{
-				if(k < 10)
-					printf("%d,   ", k);
-				else if(k < 100)
-					printf("%d,  ", k);
-				else
-					printf("%d, ", k);
+				k = j * i;
+				if (j == 0)
+					printf("%d", k);
+				else if (k < 10 && j != 0)
+					printf(",   %d", k);
+				else if (k >= 10 && k < 100)
+					printf(",  %d", k);
+				else if (k >= 100)
+					printf(", %d", k);
 			}
-			else
-				printf("%d", k);
+			printf("\n");
 		}
-		printf("\n");
 	}
 }
