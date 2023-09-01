de12877cae2d92fa375b7561c1f7c8f3eccc1819
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2023-09-01 09:40:21 +0100

    header file

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int _putchar(char c);
+int _atoi(char *s);
+
+#endif
+
