6f4ef12efbd134d762a1d32e0999eed95a4217c8
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2023-09-01 09:39:03 +0100

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - writ the programs name
+ * @argc: number of array arg
+ * @argv: arguments
+ * Return: 0 if Success
+ */
+int main(int ac, char **av)
+{
+	(void)ac;
+	printf("%s\n", av[0]);
+	return (0);
+}
