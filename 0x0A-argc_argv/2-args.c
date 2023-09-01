153c125faf6cc422f7a444238ba2f6ceac1a6f8b
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2023-09-01 09:39:30 +0100

    argc

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - writ arguments
+ * @argc: nbr arry
+ * @argv: array
+ *
+ * Return: 0 success
+ */
+int main(int ac, char **av)
+{
+	while (*av)
+		printf("%s\n", *av++);
+	return (0);
+}
